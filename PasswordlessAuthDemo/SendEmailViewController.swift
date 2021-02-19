b62fdbc57fa15520dad865d2d8a47152563d1291
mtkmr <m.takamura0520@gmail.com>
2021-02-19 19:18:10 +0900

    ダイナミックリンクの内容確認

--- PasswordlessAuthDemo/SendEmailViewController.swift
+++ PasswordlessAuthDemo/SendEmailViewController.swift
@@ -37,52 +37,10 @@ class SendEmailViewController: UIViewController {
         components.queryItems = [emailTypeQueryItem]
         guard let linkParameter = components.url else { return }
         print("I am email \(linkParameter.absoluteString)")
-//        actionCodeSettings.url = URL(string: "https://passwordlessauth.com/signin")
         actionCodeSettings.url = linkParameter
         actionCodeSettings.handleCodeInApp = true
         actionCodeSettings.setIOSBundleID(Bundle.main.bundleIdentifier!)
         
-//        //URL Componentsの作成
-//        let email = emailTextField.text ?? ""
-//
-//        var components = URLComponents()
-//        components.scheme = "https"
-//        components.host = "passwordlessauthdemo.firebaseapp.com"
-////        components.path = "/open"
-//        let queryItemEmailName = "email"
-//        let emailTypeQueryItem = URLQueryItem(name: queryItemEmailName, value: email)
-//        components.queryItems = [emailTypeQueryItem]
-//
-//        guard let linkParameter = components.url else { return }
-//        print("I am email \(linkParameter.absoluteString)")
-//
-//        //長いダイナミックリンクの作成
-//        guard let emailLink = DynamicLinkComponents(link: linkParameter, domainURIPrefix: "https://nopasswordauth.page.link") else {
-//            print("Couldn't create FDL components")
-//            return
-//        }
-//        if let myBundleId = Bundle.main.bundleIdentifier {
-//            emailLink.iOSParameters = DynamicLinkIOSParameters(bundleID: myBundleId)
-//        }
-//        emailLink.iOSParameters?.appStoreID = "1554115985"
-//
-//        guard let longURL = emailLink.url else { return }
-//        print("The long dynamic link is \(longURL.absoluteString)")
-//
-//        emailLink.shorten { (url, warnaings, err) in
-//            if let err = err {
-//                print("Oh no! Error: \(err.localizedDescription)")
-//                return
-//            }
-//            if let warnings = warnaings {
-//                for warning in warnings {
-//                    print("FDL Warning: \(warning)")
-//                }
-//            }
-//            guard let url = url else { return }
-//            print("I have a short URL to email! \(url.absoluteString)")
-//
-//        }
         
         Auth.auth().sendSignInLink(toEmail: email, actionCodeSettings: actionCodeSettings) { (err) in
             if let err = err {
